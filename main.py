4c8f84df28f41d1bde72ee5f72cadedd829ffeb6
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-05-25 17:01:21 +0300

    add

--- main.py
+++ main.py
@@ -9,7 +9,5 @@ status_code = re.get(base_url).status_code
 
 response = f'https://api.exchangeratesapi.io/v1/symbols?access_key = {API_KEY}'
 
-with open("data","w") as outfile:
+with open("data", "w") as outfile:
     json.dump(response, outfile)
-
-    
