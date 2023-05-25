0204129a24b7da837ad7a60e6b5a1f2c7fa36592
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-05-25 16:53:47 +0300

    fork

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests as re
+import json
+
+
+API_KEY = ""
+base_url = "https://api.exchangeratesapi.io/v1/"
+
+status_code = re.get(base_url).status_code
+
+response = f'https://api.exchangeratesapi.io/v1/symbols?access_key = {API_KEY}'
+
+with open("data","w") as outfile:
+    json.dump(response, outfile)
+
+    
