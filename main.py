26f403ed3e56eab159ff80dba37025e479d634ac
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-05-26 17:20:54 +0300

    update

--- main.py
+++ main.py
@@ -5,32 +5,14 @@ import json, pprint, os
 API_KEY = "3b657357d9c210c7bcee8ce37875e685"
 base_url = "https://currate.ru/"
 
-status_code = re.get(base_url).status_code
+# req_ = re.get(f'https://currate.ru/api/?get=currency_list&key={API_KEY}')
 
-get_currncy_list = re.get(f'https://currate.ru/api/?get=currency_list&key={API_KEY}')
-# g_list = get_currncy_list.text[1:-1:].replace(',', '\n')
+# req_to_json = json.loads(get_currncy_list.text)
 
-req_ = json.loads(get_currncy_list.text)
-
-with open('req.json', 'w') as outfile:
-    json.dump(req_, outfile,ensure_ascii=False,indent=4)
-
-# with open ('currensy_list', 'w') as outfile:
-#     outfile.write(get_currncy_list)
-
-
-
-# with open("currency_list.json", "w") as w_file:
-#     json.dump(get_currncy_list['data'], w_file)
-
+if os.path.exists('req.json')==0:    
+    with open('req.json', 'w') as outfile:
+        json.dump(req_to_json, outfile, ensure_ascii=False, indent=4)
 
 
 
 # get =  https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB&key=YOUR-API-KEY
-
-
-
-
-# with open("data.json", "w") as outfile:
-#     json.dump(get_currncy_list, outfile)
-# print()
