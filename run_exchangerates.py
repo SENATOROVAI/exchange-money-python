a3b0ac690f0ee67450a3c2eca6a0fddd049b0092
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-05-31 21:45:02 +0400

    Update run_exchangerates.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- run_exchangerates.py
+++ run_exchangerates.py
@@ -15,9 +15,14 @@ def exchange(cur, amount):
         result = float(req_json ПО КЛЮЧУ получить -> cur <- который вложен в rates)
     return f"{amount} EUR = "+str(result) + f" {cur}"
 
-# реализация на замыканиях
 def currency(currency):
+    """
+    This function returns a nested function that takes an 'amount' as input, 
+    extracts the exchange rate for a given 'currency' from a JSON file,
+    and returns the converted amount.
+    """
     def among(amount):
+        nonlocal currency
         cur = currency
         with open("ФАЙЛ.JSON ИЩИ В ПРОЕКТЕ", "r") as f:
             req_json = ДЖЭСОН.МЕТОД ЗАГРУЗКИ(f.прочитать)
